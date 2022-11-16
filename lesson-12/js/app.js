594ecf8a12d916bb593820caca6edda3e961e480
Dmytro Valihun <dmytro.valihun@gmail.com>
2022-11-16 17:56:01 +0100

    homework (lesson-12) is done

---
+++ lesson-12/js/app.js
@@ -0,0 +1,59 @@
+const orderForm = document.querySelector('.form');
+orderForm.hidden = true;
+const orderInfo = document.querySelector('.order-info');
+orderInfo.hidden = true;
+
+function showCategory() {
+    const container = document.querySelector('.category');
+    for (let i = 0; i < data.length; i++) {
+        const elem = document.createElement('div');
+        elem.textContent =  data[i].name;
+        elem.setAttribute('data-category', i);
+        elem.addEventListener('click', showProducts);
+        container.appendChild(elem);
+    }
+};
+
+function showProducts(event) {
+    orderForm.hidden = true;
+    orderInfo.hidden = true;
+    const resulOff = document.querySelector('.result');
+    resulOff.textContent = '';
+    const categoryIndex = event.target.getAttribute('data-category');
+    const product = data[categoryIndex].products;
+    const container = document.querySelector('.products');
+    container.innerHTML = '';
+    for (let i = 0; i < product.length; i++) {
+        const elem = document.createElement('div');
+        elem.textContent = product[i].name;
+        elem.setAttribute('data-product', i);
+        elem.setAttribute('data-category', categoryIndex);
+        elem.addEventListener('click', showDetails);
+        container.appendChild(elem);
+    }
+};
+
+function showDetails(event) {
+    const categoryIndex = event.target.getAttribute('data-category');
+    const productIndex = event.target.getAttribute('data-product');
+    const product = data[categoryIndex].products[productIndex];
+    const container = document.querySelector('.details');
+    container.innerHTML = '';
+    const elem = document.createElement('div');
+    elem.innerHTML = product.name + '<br>' + product.price + '<br>' + product.description;
+    container.appendChild(elem);
+    const button = document.createElement('button');
+    button.textContent = 'buy';
+    container.appendChild(button);
+    button.addEventListener('click', buyProduct);
+};
+
+function buyProduct() {
+    orderForm.hidden = false;
+    const product = document.querySelector('.products');
+    product.textContent = '';
+    const details = document.querySelector('.details');
+    details.textContent = '';
+};
+
+showCategory();
---
+++ lesson-12/js/data.js
@@ -0,0 +1,57 @@
+const data = [
+    {
+        name: 'Phones',
+        products: [
+            {
+                name: 'Apple iphone X',
+                price: 1300,
+                description: 'bla bla bla'
+            },
+            {
+                name: 'Samsung S8',
+                price: 1200,
+                description: 'bla bla blablaaaaaa'
+            },
+            {
+                name: 'LG',
+                price: 1400,
+                description: 'bla blaaaaaaaa bla'
+            },
+        ],
+    },
+    {
+        name: 'Laptops',
+        products: [
+            {
+                name: 'Apple Macbook',
+                price: 2500,
+                description: 'bla blaaaaaaaaaaaaaa bla'
+            },
+            {
+                name: 'DELL',
+                price: 2400,
+                description: 'bla blaaaaa bla'
+            },
+            {
+                name: 'HP',
+                price: 2300,
+                description: 'blaaaaaaaaa bla bla'
+            },
+        ],
+    },
+    {
+        name: 'Tablets',
+        products: [
+            {
+                name: 'Apple ipad',
+                price: 1500,
+                description: 'bla blaaaaaa bla'
+            },
+            {
+                name: 'Samsung tab',
+                price: 1200,
+                description: 'bla bla blaaaaaaaaaa'
+            },
+        ],
+    },
+];
