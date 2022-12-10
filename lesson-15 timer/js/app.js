9bebd24400e7745d01a83f679f6d14311180852f
Dmytro Valihun <dmytro.valihun@gmail.com>
2022-12-10 16:13:23 +0100

    extra code removed

--- lesson-15 timer/js/app.js
+++ lesson-15 timer/js/app.js
@@ -1,15 +1,3 @@
-const numsArr = [
-    './img/0.jpg', 
-    './img/1.jpg', 
-    './img/2.jpg',
-    './img/3.jpg',
-    './img/4.jpg',
-    './img/5.jpg',
-    './img/6.jpg',
-    './img/7.jpg',
-    './img/8.jpg',
-    './img/9.jpg',
-];
 function showCurrentTime() {
     const date = new Date;
     const hour = date.getHours();
