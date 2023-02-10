100cff2469b88e120a35549b5585dfbe2c3cbc66
Dmytro Valihun <108365769+dmytro-valihun@users.noreply.github.com>
2023-02-10 21:03:48 +0100

    Delete lesson-23 directory

--- lesson-23/frontend/src/js/urls.js
+++
@@ -1,5 +0,0 @@
-const BASE_URL = 'http://localhost:5000/api';
-
-export const API_CATEGORY_LIST = `${BASE_URL}/categories`;
-export const API_ITEMS_BY_CATEGORY_ID = `${BASE_URL}/products/:category`;
-export const API_ORDER_INFO = `${BASE_URL}/order`;
